b1a5fc8c1b6dea767b04401e5bdafaa5645a0b0a
nkearse1 <nckearse1@gmail.com>
2025-07-02 17:06:03 -0400

    Add props to TalentSignUpForm

--- components/TalentSignUpForm.tsx
+++ components/TalentSignUpForm.tsx
@@ -1,7 +1,14 @@
 'use client';
 import { SignUp } from '@clerk/nextjs';
 
-export function TalentSignUpForm() {
+interface TalentSignUpFormProps {
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export function TalentSignUpForm({ loading, setLoading }: TalentSignUpFormProps) {
+  void loading;
+  void setLoading;
   return (
         <SignUp signInUrl="/sign-in" redirectUrl="/talent/dashboard" />
   );
