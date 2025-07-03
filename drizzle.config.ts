5807dd49dd58c41244430aa2844f8c2d843ef263
nkearse1 <nckearse1@gmail.com>
2025-07-03 13:28:41 -0400

    Configure drizzle with pg and update db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,7 +8,7 @@ export default {
   out: './drizzle',
   driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL as string,
+    connectionString: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
