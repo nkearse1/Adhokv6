122b450f5760f3e8f248a3f879d9834eee14251a
nkearse1 <nckearse1@gmail.com>
2025-07-22 12:35:02 -0400

    fix: restrict db usage to server

--- db/index.ts
+++ db/index.ts
@@ -3,11 +3,11 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error('DATABASE_URL is not defined in environment variables');
+if (typeof process.env.DATABASE_URL !== 'string') {
+  throw new Error('DATABASE_URL is not defined in server environment');
 }
+
+const connectionString = process.env.DATABASE_URL;
 
 const sql = neon(connectionString);
 
